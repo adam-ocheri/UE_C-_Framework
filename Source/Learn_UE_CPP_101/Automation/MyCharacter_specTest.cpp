41b2d4ca9502764939f9714cfd33945d69fa7526
Adam Ocheri <adamotchery@gmail.com>
2022-11-08 04:31:56 +0200

    I ruined testing

---
+++ Source/Learn_UE_CPP_101/Automation/MyCharacter_specTest.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Automation/MyCharacter_specTest.h"
+
+MyCharacter_specTest::MyCharacter_specTest()
+{
+}
+
+MyCharacter_specTest::~MyCharacter_specTest()
+{
+}
---
+++ Source/Learn_UE_CPP_101/Automation/MyCharacter_specTest.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class LEARN_UE_CPP_101_API MyCharacter_specTest
+{
+public:
+	MyCharacter_specTest();
+	~MyCharacter_specTest();
+};
