bb9bf531a16b3fee9f96b13af5f174f6f0ea90d9
Adam Ocheri <adamotchery@gmail.com>
2022-11-08 02:48:27 +0200

    tried to get rid of red v's in c++ classes as seen in the editor

---
+++ Source/Learn_UE_CPP_101/Automation/AutoTests.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Automation/AutoTests.h"
+
+AutoTests::AutoTests()
+{
+}
+
+AutoTests::~AutoTests()
+{
+}
---
+++ Source/Learn_UE_CPP_101/MyTest1.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyTest1.h"
+
+MyTest1::MyTest1()
+{
+}
+
+MyTest1::~MyTest1()
+{
+}
